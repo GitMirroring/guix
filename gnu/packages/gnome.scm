2645caa13c27ec28a283a54d52e556bd34fa6278
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2025-05-19 14:01:21 +0900

    gnu: gnome-online-accounts: Update to 3.54.4.
    * gnu/packages/gnome.scm (gnome-online-accounts): Update to 3.54.4.
    [#:configure-flags]: Add “-Dc-args=-Wno-error=missing-include-dirs”.
    [native-inputs]: Add gi-docgen.
    [inputs]: Add keyutils.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -119,6 +119,7 @@
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages cpp)
+  #:use-module (gnu packages crypto)
   #:use-module (gnu packages cups)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages cyrus-sasl)
@@ -8142,7 +8143,7 @@ window manager.")
 (define-public gnome-online-accounts
   (package
     (name "gnome-online-accounts")
-    (version "3.50.7")
+    (version "3.54.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -8150,11 +8151,12 @@ window manager.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1y4dn60vm1dy9hajqbm7hvzs9j2468rxnyg5ma3xx53mssq885kd"))))
+                "14zm3innz8rflvzazqcm2bvh503xfv1a81x6bs4b9p27wx1rjwsm"))))
     (build-system meson-build-system)
     (arguments
      (list
       #:glib-or-gtk? #t
+      #:configure-flags #~(list "-Dc_args=-Wno-error=missing-include-dirs")
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'disable-gtk-update-icon-cache
@@ -8166,6 +8168,7 @@ window manager.")
      (list gettext-minimal
            `(,glib "bin")               ; for glib-compile-schemas, etc.
            gobject-introspection
+           gi-docgen
            libxslt
            pkg-config
            vala))
@@ -8177,6 +8180,7 @@ window manager.")
            dbus
            gcr
            json-glib
+           keyutils
            libsecret
            mit-krb5
            rest-next
