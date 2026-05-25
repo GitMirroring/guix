35464b40a40187de8742d65e4478db92e90032a7
SilverlightningY <reband.florian@googlemail.com>
2026-05-25 02:27:38 +0200

    gnu: ffmpeg: Remove frei0r-plugins.
    * gnu/packages/video.scm (ffmpeg)[arguments]<#:phases>{wrap-executable}:
      Remove FREI0R_PATH wrapping.
      [native-inputs]: Add frei0r-api.
      [inputs]: Remove frei0r-plugins.
      [native-search-paths]: Add FREI0R_PATH.
    
    Change-Id: Icc4e41258b70062345a88dbcaaa83d1496aa2370
    Reviewed-by: Efraim Flashner <efraim@flashner.co.il>
    Reviewed-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Reviewed-by: Sughosha <sughosha@disroot.org>
    Signed-off-by: Nguyễn Gia Phong <cnx@loang.net>

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1782,7 +1782,6 @@ operate properly.")
       (list dav1d
             fontconfig
             freetype
-            frei0r-plugins
             gnutls
             opus
             ladspa
@@ -1821,6 +1820,7 @@ operate properly.")
             zlib)))
     (native-inputs
      (list bc
+           frei0r-api
            perl
            pkg-config
            texinfo
@@ -1983,6 +1983,10 @@ filter-scale2ref_keep_aspect,sws-floatimg-cmp")
                      (path (string-join (map dirname dso) ":")))
                 (format #t "setting LD_LIBRARY_PATH to ~s~%" path)
                 (setenv "LD_LIBRARY_PATH" path)))))))
+    (native-search-paths
+     (list (search-path-specification
+            (variable "FREI0R_PATH")
+            (files (list "lib/frei0r-1")))))
     (home-page "https://www.ffmpeg.org/")
     (synopsis "Audio and video framework")
     (description "FFmpeg is a complete, cross-platform solution to record,
